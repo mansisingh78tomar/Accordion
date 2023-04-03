a79e8a4ac5219a6e1ae715841ac2de3c0bec7c48
mansisingh78tomar <118250409+mansisingh78tomar@users.noreply.github.com>
2023-04-03 22:00:03 +0530

    Accordion

---
+++ main.js
@@ -0,0 +1,8 @@
+const accordion = document.getElementsByClassName('accordion');
+
+
+for(let i = 0; i < accordion.length; i++){
+    accordion[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+    });
+}
